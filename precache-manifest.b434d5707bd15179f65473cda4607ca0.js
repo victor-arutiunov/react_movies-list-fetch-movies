027341d3566a094f8028e5f172bb2cba8d2a5fd2
victor <victorarytionowmail@gmail.com>
2021-06-03 20:27:12 +0300

    Updates

---
+++ precache-manifest.b434d5707bd15179f65473cda4607ca0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4b65cbefe4a04d23e6009bfce6e32a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3d747f37958917715700",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "28f02ff693896698dac0",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "3d747f37958917715700",
+    "url": "./static/js/2.adbbac91.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.adbbac91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28f02ff693896698dac0",
+    "url": "./static/js/main.6f02ddb8.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
